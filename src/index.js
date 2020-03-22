d3c7984f4585d1793a639ed01b83aff0147a0d07
TimurIskandarov <tim-darov@rambler.ru>
2020-03-22 19:45:00 +0400

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -39,6 +39,34 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const lengthLetter = 10;
+    let wordsMorse = [];
+    let charsMorse = [];
+    for(let i = j = 0; i < expr.length; j++) {
+        charsMorse.push(expr.slice(i, i+lengthLetter));
+        i += lengthLetter;
+        let letter = charsMorse[j];
+        let symbol = '';
+        let codeSymbol= '';
+        for(let k = 0; k < lengthLetter; k ++) {
+            codeSymbol = letter[k] + letter[k+1];
+            switch(codeSymbol){
+                case('10'): { symbol += '.'; k += 1; break; }
+                case('11'): { symbol += '-'; k += 1; break; }
+                case('**'): { symbol += ' '; k += lengthLetter; break; }
+                default: continue;
+            }
+        }
+        wordsMorse.push(symbol);
+        symbol = '';
+    }
+    // for(let char of charsMorse)
+    //     console.log(char); 
+    let res = '';        
+    for(let word of wordsMorse) {
+        res += word != ' ' ? MORSE_TABLE[word] : ' ';
+    }
+    return res;
 }
 
 module.exports = {
